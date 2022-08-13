a74f59fb2f2fe9764d8ff355193c24e6279a1a8d
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2022-08-12 19:07:14 -0500

    Initial code checkin

---
+++ src/sttp/README.md
@@ -0,0 +1,5 @@
+![Logo](../../docs/img/LockPython_64High.png)![Banner](https://raw.githubusercontent.com/sttp/pyapi/main/docs/img/sttp.png)
+
+# STTP Implementation in Python
+
+This Python based STTP implementation can acquire metadata and expose it as a cache (see [metadataCache.py](metadataCache.py)).
---
+++ tests/main.py
@@ -0,0 +1,82 @@
+#******************************************************************************************************
+#  main.py - Gbtc
+#
+#  Copyright © 2022, Grid Protection Alliance.  All Rights Reserved.
+#
+#  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+#  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+#  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+#  file except in compliance with the License. You may obtain a copy of the License at:
+#
+#      http://opensource.org/licenses/MIT
+#
+#  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+#  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+#  License for the specific language governing permissions and limitations.
+#
+#  Code Modification History:
+#  ----------------------------------------------------------------------------------------------------
+#  08/12/2022 - J. Ritchie Carroll
+#       Generated original version of source code.
+#
+#******************************************************************************************************
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
+
+from sttp.connection import connection
+from sttp.measurementRecord import SignalType
+from sttp.metadataCache import metadataCache
+from gsf import Ticks
+from typing import Optional, List
+from datetime import datetime, timedelta
+from time import time, sleep
+import numpy as np
+
+def main():
+    print("Creating STTP API")
+    
+    sttpapi = connection("localhost")
+
+    print("Connecting...")
+    
+    try:
+        sttpapi.Connect()
+        
+        if sttpapi.IsConnected:
+            print(f"Connected to \"{sttpapi.HostAddress}\"!")
+
+            # Suppress default output with: sttpapi.RefreshMetadata(logOutput = lambda value: None)
+            recordCount = sttpapi.RefreshMetadata()
+                
+            # Get a reference to the STTP metadata cache
+            metadata = sttpapi.Metadata
+
+            # Lookup measurements for frequency signals
+            records = metadata.GetMeasurementsBySignalType(SignalType.FREQ)
+
+            # Lookup measurements for voltage phase magnitudes
+            #records.extend(metadata.GetMeasurementsBySignalType(SignalType.VPHM, instance.Name))
+
+            # Lookup devices by matching text fields
+            #for device in metadata.GetDevicesByTextSearch("WESTPNT"):
+            #    records.extend(device.Measurements)
+
+            recordCount = len(records)
+
+            print(f"Queried {recordCount:,} metadata records.")
+        else:
+            print("Not connected? Unexpected.")
+    except Exception as ex:
+        print(f"Failed to connect: {ex}")
+    finally:
+        if sttpapi.IsConnected:
+            print("Disconnecting.")
+        
+        sttpapi.Disconnect()
+
+
+if __name__ == "__main__":
+    main()
