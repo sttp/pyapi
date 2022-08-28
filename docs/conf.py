ad0df1e429d2ac9304645088239a72b44eef8116
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2022-08-28 17:39:03 -0500

    Updated jeykll yml to include Sphinx folders

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'STTP pyapi'
+project = 'sttpapi'
 copyright = '2022, Grid Protection Alliance'
 author = 'J. Ritchie Carroll'
 
