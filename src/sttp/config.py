033a699b1857860284953492c9ed5953daa52f22
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2025-06-19 12:30:01 -0500

    Updated tying for settings/config defaults

--- src/sttp/config.py
+++ src/sttp/config.py
@@ -21,6 +21,7 @@
 #
 # ******************************************************************************************************
 
+from types import EllipsisType
 from .transport.constants import Defaults
 import numpy as np
 
@@ -43,18 +44,18 @@ class Config:
     DEFAULT_VERSION = Defaults.VERSION
 
     def __init__(self,
-                 maxretries: int = ...,
-                 retryinterval: float = ...,
-                 maxretryinterval: float = ...,
-                 autoreconnect: bool = ...,
-                 autorequestmetadata: bool = ...,
-                 autosubscribe: bool = ...,
-                 compress_payloaddata: bool = ...,
-                 compress_metadata: bool = ...,
-                 compress_signalindexcache: bool = ...,
-                 metadatafilters: str = ...,
-                 socket_timeout: float = ...,
-                 version: np.byte = ...
+                 maxretries: int | EllipsisType = ...,
+                 retryinterval: float | EllipsisType = ...,
+                 maxretryinterval: float | EllipsisType = ...,
+                 autoreconnect: bool | EllipsisType = ...,
+                 autorequestmetadata: bool | EllipsisType = ...,
+                 autosubscribe: bool | EllipsisType = ...,
+                 compress_payloaddata: bool | EllipsisType = ...,
+                 compress_metadata: bool | EllipsisType = ...,
+                 compress_signalindexcache: bool | EllipsisType = ...,
+                 metadatafilters: str | EllipsisType = ...,
+                 socket_timeout: float | EllipsisType = ...,
+                 version: np.byte | EllipsisType = ...
                 ):
         """
         Creates a new `Config` instance.
